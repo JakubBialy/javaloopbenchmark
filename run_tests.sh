793298e9c010eff09ef0085c3af1de6072fbdf19
Jakub Bialy <bialyjakubpoczta@gmail.com>
2020-08-29 01:42:47 +0200

    Fixes

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 JDKS=($LT_JAVA_8 $LT_JAVA_11 $LT_JAVA_12 $LT_JAVA_13 $LT_JAVA_14 $LT_JAVA_15 $LT_JAVA_16)
 
-timestamp=$(date +"%Y%m%d%_k%M%N")
+timestamp=$(date +"%Y%m%d%H%M%N")
 outputDir=./data/$timestamp
 mkdir -p $outputDir
 
@@ -18,8 +18,6 @@ do
         printf "Java major version: $curr_java_major_ver\n\n"
         JAVA_HOME=$(echo $item) mvn -Djdk.ver=$curr_java_major_ver clean package
 
-        echo "==="
-        echo "$item/bin/java -jar ./target/Java_Benchmarks-0.1.0.jar IteratingBenchmark -jvmArgs -XX:+UseG1GC -bm thrpt -wi 10 -w 5 -r 10 -i 30 -f 1 -rf scsv -rff $outputDir/benchmark_$curr_java_ver.scsv"
         $item/bin/java -jar ./target/Java_Benchmarks-0.1.0.jar IteratingBenchmark -jvmArgs -XX:+UseG1GC -bm thrpt -wi 10 -w 5 -r 10 -i 30 -f 1 -rf scsv -rff $outputDir/benchmark_$curr_java_ver.scsv
 done
 
